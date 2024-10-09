696c0f92049b442fed304eeac905fb47e615d85e
jurraca <julienu@pm.me>
2024-10-09 15:15:06 +0100

    get scripts working

--- flake.nix
+++ flake.nix
@@ -13,22 +13,22 @@
 
       run-local-pool = pkgs.writeScriptBin "run-local-pool"
         ''
-        cargo run -- -c roles/pool/config-examples/pool-config-local-tp-example.toml
+        cargo -C roles/pool -Z unstable-options run -- -c roles/pool/config-examples/pool-config-local-tp-example.toml
 	'';
 
       run-job-server = pkgs.writeScriptBin "run-job-server"
         ''
-        cargo run -- -c roles/jd-server/config-examples/jds-config-local-example.toml
+        cargo -C roles/jd-server -Z unstable-options run -- -c roles/jd-server/config-examples/jds-config-local-example.toml
 	'';
 
       run-job-client = pkgs.writeScriptBin "run-job-client"
         ''
-        cargo run -- -c roles/jd-client/config-examples/jds-config-local-example.toml
+        cargo -C roles/jd-client -Z unstable-options run -- -c roles/jd-client/config-examples/jds-config-local-example.toml
 	'';
 
       run-translator-proxy = pkgs.writeScriptBin "run-translator-proxy"
         ''
-        cargo run -- -c roles/translator/config-examples/tproxy-config-local-jdc-example.toml
+        cargo -C roles/translator -Z unstable-options run -- -c roles/translator/config-examples/tproxy-config-local-jdc-example.toml
 	'';
       run-bitcoind = pkgs.writeScriptBin "run-bitcoind"
         ''
